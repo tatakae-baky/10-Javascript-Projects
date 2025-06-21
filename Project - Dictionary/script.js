0ba2b0366ac708a4197dc25abe3fedf8efa777b2
J.AL BAKY <jubairalbaky12@gmail.com>
2025-06-21 12:29:01 +0600

    added robust example method

--- Project - Dictionary/script.js
+++ Project - Dictionary/script.js
@@ -5,13 +5,19 @@ const sound = document.getElementById("sound");
 btn.addEventListener("click", () => {
   const input_word = document.getElementById("input-word").value;
   const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${input_word}`;
-  
+
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      
       if (data && data[0] && data[0].meanings && data[0].meanings[0]) {
+        let example = "";
+        for (const definition of data[0].meanings[0].definitions) {
+          if (definition.example) {
+            example = definition.example;
+            break;
+          }
+        }
+        
         result.innerHTML = `
           <div class="word">
             <h3>${data[0].word}</h3>
@@ -21,16 +27,20 @@ btn.addEventListener("click", () => {
           </div>
           <div class="details">
             <p>${data[0].meanings[0].partOfSpeech}</p>
-            <p>/${data[0].phonetic || ''}/</p>
+            <p>/${data[0].phonetic || ""}/</p>
           </div>
           <p class="word-meaning">
             ${data[0].meanings[0].definitions[0].definition}
           </p>
           <p class="word-example">
-            ${data[0].meanings[0].definitions[0].example || data[0].meanings[0].definitions[1].example || 'No example available'}
+            ${example}
           </p>`;
-        
-        if (data[0].phonetics && data[0].phonetics[0] && data[0].phonetics[0].audio) {
+
+        if (
+          data[0].phonetics &&
+          data[0].phonetics[0] &&
+          data[0].phonetics[0].audio
+        ) {
           sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
         }
       } else {
@@ -41,7 +51,6 @@ btn.addEventListener("click", () => {
       result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
     });
 });
-
 
 function playSound() {
   sound.play();
